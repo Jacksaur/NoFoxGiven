66506abcda19248480c58a507e4f3b2d741be65b
Jacksaur <58850292+Jacksaur@users.noreply.github.com>
2022-03-29 13:13:30 +0100

    Create custom /bin file to preserve KDE Filepicker tweak
    Now with Comments.

--- NoFoxGiven.sh
+++ NoFoxGiven.sh
@@ -2,13 +2,17 @@
 echo "NoFoxGiven Snap removal and replacement script for Firefox."
 echo "WARNING: If you have not backed up your Firefox profile, **EVERYTHING WILL BE LOST**. Close this command prompt and check the Github page for instructions on how to do so. Otherwise, press Enter to continue."
 read
+
+#Check if Curl is installed, and abort if not.
 if ! command -v curl &> /dev/null
 then
     echo "curl is not installed on your system. Install it first with sudo apt install curl, then restart this script."
     read
     exit
 fi
+
 echo "Now downloading latest Standalone Firefox build"
+#Direct download link to latest Firefox version from Mozilla
 curl -L -o firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
 echo "Extracting..."
 tar xjf firefox.tar.bz2
@@ -16,7 +20,11 @@ rm -r ./firefox.tar.bz2
 echo "Moving files and adding Launcher entry. You will be asked for your sudo pass."
 sleep 3
 mv ./firefox ~/.local/share/Firefox
-ln -s ~/.local/share/Firefox/firefox ~/.local/bin
+#Create /bin file to allow launching through Terminal, whilst still applying KDE Filepicker tweak
+touch ~/.local/bin/firefox
+echo "#!/bin/bash" > ~/.local/bin/firefox
+echo "GTK_USE_PORTAL=1 ~/.local/share/Firefox/firefox" >> ~/.local/bin/firefox
+#Copy Icons and .desktop file to their proper places
 sudo rsync -a ./icons /usr/share
 sudo mv ./Firefox.desktop /usr/share/applications/
 rm -r ./icons
